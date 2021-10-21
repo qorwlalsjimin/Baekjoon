188f69ddca1556d6835d6508d9172a5f77f0ec86
Baek Jimin <s2104@e-mirim.hs.kr>
2021-10-21 13:17:53 +0900

    계산 단축

--- Array/Q1546.java
+++ Array/Q1546.java
@@ -9,24 +9,19 @@ public class Q1546 {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
 		int[] score = new int[n];
-		double[] result = new double[score.length];
 		
 		for(int i = 0; i<score.length; i++)
 			score[i] = scan.nextInt();
 		
+		double sum = 0.0;
 		int max = 0;
 		for(int i = 0; i<score.length; i++) {
-		if(score[i]>max)
-			max = score[i];
+			if(score[i]>max)
+				max = score[i];
+			sum+=score[i];
 		}//for
 
-		float sum = 0;
-		for(int i = 0; i<score.length; i++) {
-			result[i] = ((double)score[i]/(double)max)*100;
-			sum += result[i];
-		}
-
-		double average = (double)sum/n;
+		double average = (double)sum/max*100/n;
 		System.out.println("*"+average);
 
 	}
